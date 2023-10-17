76d21ecd51da2bed309db690ea7478888f3510d8
blessdyb <blessdyb@gmail.com>
2023-10-16 21:55:33 -0700

    Update asset prefix

--- src/components/IconCharts.js
+++ src/components/IconCharts.js
@@ -9,49 +9,59 @@ const formatNumber = (number) => {
   }
 };
 
-const PersonIcon = ({ value = 0, label = 0, race = '', scale = 1, onDisclaimerChange = () => {} }) => {
+const PersonIcon = ({
+  value = 0,
+  label = 0,
+  race = "",
+  scale = 1,
+  onDisclaimerChange = () => {},
+}) => {
   const valueRoof = Math.ceil(value);
   const maskHeight = {
     height: `${valueRoof * 100 - value * 100}%`,
   };
   if (valueRoof === 0 && scale !== 1) {
-    onDisclaimerChange("N/A")
+    onDisclaimerChange("N/A");
   } else if (scale == 1 && valueRoof === 0) {
-    onDisclaimerChange("0.0")
+    onDisclaimerChange("0.0");
   }
   return (
     <div className="icon-chart-data">
-      {((valueRoof > 0 && label >= 10) || scale == 1) ? (
-        valueRoof === 0 ? (<div className="icon-chart-data-point">
-        <div className="icon-person icon-person-placeholder" />
-        <span className="icon-chart-data-point-mask">
-          <div className="icon-chart-data-label">0.0</div>
-        </span>
-      </div>) : Array(valueRoof)
-          .fill(0)
-          .map((_, index) => {
-            return (
-              <div className="icon-chart-data-point" key={index}>
-                <svg
-                  aria-label="Person"
-                  className={`icon-person re-${race}`}
-                  title={valueRoof}
-                >
-                  <use href="/images/sprites.svg#person"></use>
-                </svg>
-                <span
-                  className="icon-chart-data-point-mask"
-                  style={valueRoof - 1 === index ? maskHeight : {}}
-                >
-                  {valueRoof - 1 === index && (
-                    <div className="icon-chart-data-label">
-                      {formatNumber(label)}
-                    </div>
-                  )}
-                </span>
-              </div>
-            );
-          })
+      {(valueRoof > 0 && label >= 10) || scale == 1 ? (
+        valueRoof === 0 ? (
+          <div className="icon-chart-data-point">
+            <div className="icon-person icon-person-placeholder" />
+            <span className="icon-chart-data-point-mask">
+              <div className="icon-chart-data-label">0.0</div>
+            </span>
+          </div>
+        ) : (
+          Array(valueRoof)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <div className="icon-chart-data-point" key={index}>
+                  <svg
+                    aria-label="Person"
+                    className={`icon-person re-${race}`}
+                    title={valueRoof}
+                  >
+                    <use href="https://blessdyb.github.io/paperprison-rjatool/images/sprites.svg#person"></use>
+                  </svg>
+                  <span
+                    className="icon-chart-data-point-mask"
+                    style={valueRoof - 1 === index ? maskHeight : {}}
+                  >
+                    {valueRoof - 1 === index && (
+                      <div className="icon-chart-data-label">
+                        {formatNumber(label)}
+                      </div>
+                    )}
+                  </span>
+                </div>
+              );
+            })
+        )
       ) : (
         <div className="icon-chart-data-point">
           <div className="icon-person icon-person-placeholder" />
@@ -65,9 +75,11 @@ const PersonIcon = ({ value = 0, label = 0, race = '', scale = 1, onDisclaimerCh
 };
 
 const CHART_DISCLAIMER = {
-  "N/A": "N/A: Our tool displays N/A when when there are 10 or less underlying observations.",
-  "0.0": "Anywhere a disparity gap reads 0.0, it means that there were no white adults in the system at that point for a comparison of rates. Please see raw numbers and rates to see how adults of color are impacted by the decision point.",
-}
+  "N/A":
+    "N/A: Our tool displays N/A when when there are 10 or less underlying observations.",
+  "0.0":
+    "Anywhere a disparity gap reads 0.0, it means that there were no white adults in the system at that point for a comparison of rates. Please see raw numbers and rates to see how adults of color are impacted by the decision point.",
+};
 
 const SCALE = {
   100: 10,
@@ -80,35 +92,42 @@ const SCALE = {
   500000: 50000,
   1000000: 100000,
   5000000: 500000,
-  10000000: 1000000
-}
+  10000000: 1000000,
+};
 
 const getScale = (data) => {
-  let max = 0
-  data.forEach(({items}) => {
-    max = Math.max(...Object.values(items), max)
-  })
-  const list = Object.keys(SCALE).map(item => parseInt(item, 10)).concat([max])
+  let max = 0;
+  data.forEach(({ items }) => {
+    max = Math.max(...Object.values(items), max);
+  });
+  const list = Object.keys(SCALE)
+    .map((item) => parseInt(item, 10))
+    .concat([max]);
   return {
     max,
-    scale: SCALE[`${list[list.sort((a,b) => a - b).indexOf(max) + 1]}`]
-  }
-}
+    scale: SCALE[`${list[list.sort((a, b) => a - b).indexOf(max) + 1]}`],
+  };
+};
 
 const IconCharInner = ({ chartData, races, base, measurement }) => {
-  const [disclaimer, setDisclaimer] = useState('')
+  const [disclaimer, setDisclaimer] = useState("");
   let scale = 1;
   const isRawNumber = [
     "Raw numbers",
     "Rate per population",
     "Rate per prior event point",
   ].includes(measurement);
-  const yearData = JSON.parse(JSON.stringify({
-    ...chartData,
-    ...getScale(chartData.data),
-  }));
-  if (measurement === "Raw numbers" || measurement === "Rate per prior event point") {
-    scale = yearData.scale 
+  const yearData = JSON.parse(
+    JSON.stringify({
+      ...chartData,
+      ...getScale(chartData.data),
+    })
+  );
+  if (
+    measurement === "Raw numbers" ||
+    measurement === "Rate per prior event point"
+  ) {
+    scale = yearData.scale;
   }
   const scaledYearData = yearData.data.map((yd) => {
     return {
@@ -133,7 +152,8 @@ const IconCharInner = ({ chartData, races, base, measurement }) => {
         {yearData.year}
         <div className="chart-meta">
           <div className="chart-scale">
-            <PersonIcon value={1} race={base} scale={1} /> {scale.toLocaleString()}{" "}
+            <PersonIcon value={1} race={base} scale={1} />{" "}
+            {scale.toLocaleString()}{" "}
             {!base ? (scale > 1 ? "Adults" : "Adult ") : "White Adult"}
           </div>
         </div>
@@ -148,7 +168,7 @@ const IconCharInner = ({ chartData, races, base, measurement }) => {
                 <div className="icon-chart-rows">
                   {base && (
                     <div className="icon-chart-row">
-                      <PersonIcon value={1} race={base} scale={1} label={1}/>
+                      <PersonIcon value={1} race={base} scale={1} label={1} />
                     </div>
                   )}
                   {scaledYearData.map((raceData, raceDataIndex) => {
@@ -170,8 +190,15 @@ const IconCharInner = ({ chartData, races, base, measurement }) => {
             );
           })}
       </div>
-      {CHART_DISCLAIMER[disclaimer] && (<p className="icon-chart-disclaimer">{CHART_DISCLAIMER[disclaimer]}</p>)}
-      {measurement.indexOf("Disparity gap") > -1 && (<p className="icon-chart-disclaimer">No disparity gap information is available for arrests because they are the beginning of the process.</p>)}
+      {CHART_DISCLAIMER[disclaimer] && (
+        <p className="icon-chart-disclaimer">{CHART_DISCLAIMER[disclaimer]}</p>
+      )}
+      {measurement.indexOf("Disparity gap") > -1 && (
+        <p className="icon-chart-disclaimer">
+          No disparity gap information is available for arrests because they are
+          the beginning of the process.
+        </p>
+      )}
     </div>
   );
 };
@@ -192,7 +219,4 @@ const IconCharts = ({ data, races, base, measurement }) => {
   );
 };
 
-export {
-  IconCharts,
-  PersonIcon,
-}
+export { IconCharts, PersonIcon };
